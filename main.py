3c7f9c4c81bb5718a7160d21a7e2a1aeb8993c01
mars4049ai <barinovmarsel77@gmail.com>
2024-07-19 15:37:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,7 @@ num2 = st.text_input('Введите свой вопрос:', step=1)
 
 operation = st.selectbox('Выберите операцию:', ['Сложение', 'Вычитание', 'Умножение', 'Деление'])
 
-result = 0
+result = 12
 
-if operation == 'Сложение':
-    result = num1 + num2
-elif operation == 'Вычитание':
-    result = num1 - num2
-elif operation == 'Умножение':
-    result = num1 * num2
-elif operation == 'Деление':
-    if num2 != 0:
-        result = num1 / num2
-    else:
-        st.error('На ноль делить нельзя!')
 
 st.write(f'Результат операции {operation}: {result}')
