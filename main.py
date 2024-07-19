3e039db1dc57e048e337ecc859de41aeb61c1377
mars4049ai <barinovmarsel77@gmail.com>
2024-07-19 15:35:21 +0300

    main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title('Простой калькулятор')
 
-num1 = st.number_input('Введите первое число:', step=1)
-num2 = st.number_input('Введите второе число:', step=1)
+num1 = st.text_input('Введите Вашу роль в семье:', step=1)
+num2 = st.text_input('Введите свой вопрос:', step=1)
 
 operation = st.selectbox('Выберите операцию:', ['Сложение', 'Вычитание', 'Умножение', 'Деление'])
 
