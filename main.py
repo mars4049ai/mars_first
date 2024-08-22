032505de1845245dcd31684281ac0e038feb8a1d
mars4049ai <barinovmarsel77@gmail.com>
2024-08-22 19:55:19 +0300

    main.py

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.title('Простой калькулятор')
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
 
-num1 = st.number_input('Введите первое число:', step=1)
-num2 = st.number_input('Введите второе число:', step=1)
-
-operation = st.selectbox('Выберите операцию:', ['Сложение', 'Вычитание', 'Умножение', 'Деление'])
-
-result = 0
-
-if operation == 'Сложение':
-    result = num1 + num2
-elif operation == 'Вычитание':
-    result = num1 - num2
-elif operation == 'Умножение':
-    result = num1 * num2
-elif operation == 'Деление':
-    if num2 != 0:
-        result = num1 / num2
-    else:
-        st.error('На ноль делить нельзя!')
-
-st.write(f'Результат операции {operation}: {result}')
