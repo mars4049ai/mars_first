24ed11921e410694574591e31801d9c04f33ed00
mars4049ai <barinovmarsel77@gmail.com>
2024-08-22 20:15:44 +0300

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import streamlit as st
-import pandas as pd
-
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-
+import stremlit as st
+st.title("SEX")
+uploaded_file = st.file_uploader("Choose a file")
