98bef8f009faf3c6c1c8db1b39e4209aa104d419
mars4049ai <barinovmarsel77@gmail.com>
2024-08-22 20:28:42 +0300

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,4 @@ import streamlit as st
 number = st.number_input(
     "Insert a number", value=None, placeholder="Type a number..."
 )
-number1 = st.number_input(
-    "Insert a number", value=None, placeholder="Type a number..."
-)
-st.write(number + number1)
+st.write(number + 1000)
